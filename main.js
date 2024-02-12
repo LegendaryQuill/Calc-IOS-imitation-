a2eb6c73b320b450e6d474ccac0f3b0b9db7bd93
LegendaryQuill <rasmuslumbye@gmail.com>
2024-02-13 00:25:56 +0100

    minor comment changes

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-/* When pressing a button this should be shown on the display
-    1) [make a funtion to store the equation and show the last active number "before operator" in the display]
-        [bridge between HTML and JavaScript]
-            
-   when pressing an operator it should not be shown. When pressing a new number the current number should be swapped.
-    2) [ make a placeholder for operator to track if one is pressed - range from 0-1]
-    
-    When pressing "=" the calculation is shown in the display. If i on the other hand press an operator after one operator has allready been pressed, the solution is shown.
-    3) [make a function that solves the equation (display and solve)]
-    
-    When pressing ="+/-" and "%" the calculations is made at an instance
-    4) [make individual functions for "clear", "minus/plus conversion" and "percentage"]
-
-    Make styling when pressing
-    5) keep active button pressed - not equal. */
+// IOS calculator APP that mimics the IOS calculator
 
 let currentDisplay = "0"; //current display content
 let resultDisplay = false; // result display content
